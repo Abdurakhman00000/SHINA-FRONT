0b0647a2b714f286ec9bf35826ee43149b072dbd
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-01-28 21:51:51 +0600

    add some things gor structure

---
+++ src/components/ui/modals/autobodbor_modal/AutoPodbor_modal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import scss from "./AutoPodbor_modal.module.scss"
+
+const AutoPodbor_modal = () => {
+  return (
+    <div>AutoPodbor_modal</div>
+  )
+}
+
+export default AutoPodbor_modal
---
+++ src/components/ui/modals/catalog_modal/Catalog_modal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import scss from "./Catalog_modal.module.scss"
+
+const Catalog_modal = () => {
+  return (
+    <div>Catalog_modal</div>
+  )
+}
+
+export default Catalog_modal
