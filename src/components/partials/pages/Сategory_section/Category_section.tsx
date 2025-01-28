14d66f350ad96642a4c454eac2a0d51323670db8
devAlen01 <alenermekuulu@gmail.com>
2025-01-28 14:15:38 +0600

    Product_card.tsx

--- src/components/partials/pages/Сategory_section/Category_section.tsx
+++ src/components/partials/pages/Сategory_section/Category_section.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import scss from "./Category.module.scss"
+import React from "react";
+import scss from "./Category.module.scss";
+import Product_card from "@/components/ui/cards/product_card/Product_card";
 
 const Category_section = () => {
   return (
     <section className={scss.Main}>
-        <div className="container">
-            <div className={scss.content}>
-                <h1>Category_section</h1>
-            </div>
+      <div className="container">
+        <div className={scss.content}>
+          <h1>Category_section</h1>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Product_card />
+            <Product_card />
+            <Product_card />
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Category_section
+export default Category_section;
