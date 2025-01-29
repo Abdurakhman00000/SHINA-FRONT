e4c0d34c65b04785d8fdd24d82f7cb0e375cb633
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-01-29 20:38:02 +0600

    fixed some bugs

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,93 +1,81 @@
 "use client";
 
-import React, { useState } from 'react'
-import scss from "./Header.module.scss"
+import React, { useState } from "react";
+import scss from "./Header.module.scss";
 import Image from "next/image";
 import shina_logo from "../../../../public/assets/logo/shina-logo-v2.avif";
 import Category_button from "@/components/ui/buttons/category_button/Category_button";
 import Input from "@/components/ui/input/Input";
 import { LuGitCompare } from "react-icons/lu";
 import { GrFavorite } from "react-icons/gr";
-import { MdOutlinePlace } from "react-icons/md";
-import { MdOutlineDiscount } from "react-icons/md";
+import { MdOutlinePlace, MdOutlineDiscount } from "react-icons/md";
 import { FaFire } from "react-icons/fa6";
 import AutoPodbor_modal from "@/components/ui/modals/autobodbor_modal/AutoPodbor_modal";
-import CatalogModal from '@/components/ui/modals/catalog_modal/Catalog_modal';
-
+import CatalogModal from "@/components/ui/modals/catalog_modal/Catalog_modal";
 
 const Header = () => {
-
   const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = () => {
     setIsOpen((prev) => !prev);
   };
+
+  const handleModalOpen = () => setIsModalOpen(true);
+  const handleModalClose = () => setIsModalOpen(false);
 
   return (
     <header className={scss.Header}>
       <div className="container">
         <div className={scss.content}>
           <div className={scss.logo}>
-            <Image
-              src={shina_logo}
-              alt=""
-              width={350}
-              height={700}
-            />
+            <Image src={shina_logo} alt="Logo" width={350} height={700} />
           </div>
 
           <div className={scss.category_button}>
-          <Category_button isOpen={isOpen} onClick={toggleModal} />
+            <Category_button isOpen={isOpen} onClick={toggleModal} />
           </div>
 
-            <div className={scss.category_button}>
-              <Category_button />
-            </div>
-
-            <div className={scss.search}>
-              <Input />
-            </div>
-
-            <div className={scss.features}>
-              <div className={scss.favorite_icon}>
-                <GrFavorite /> <p>Избранные</p>
-              </div>
-              <div className={scss.compare_icon}>
-                <LuGitCompare /> <p>Сравнение</p>
-              </div>
-            </div>
+          <div className={scss.search}>
+            <Input />
           </div>
 
-          <div className={scss.bottom_header}>
-            <div className={scss.find_place}>
-              <button>
-                {" "}
-                <MdOutlinePlace /> Москва
-              </button>
+          <div className={scss.features}>
+            <div className={scss.favorite_icon}>
+              <GrFavorite /> <p>Избранные</p>
             </div>
-
-            <div className={scss.nav_links}>
-              <ul>
-                <li onClick={handleModalOpen}>
-                  <FaFire /> Подбор по авто
-                </li>
-                <li>
-                  {" "}
-                  <MdOutlineDiscount /> Скидки дня
-                </li>
-                <li>Блог</li>
-                <li>Бренды</li>
-                <div className={scss.line}></div>
-                <li>Шины</li>
-                <li>Диски</li>
-              </ul>
+            <div className={scss.compare_icon}>
+              <LuGitCompare /> <p>Сравнение</p>
             </div>
           </div>
         </div>
-      </header>
+
+        <div className={scss.bottom_header}>
+          <div className={scss.find_place}>
+            <button>
+              <MdOutlinePlace /> Москва
+            </button>
+          </div>
+
+          <div className={scss.nav_links}>
+            <ul>
+              <li onClick={handleModalOpen}>
+                <FaFire /> Подбор по авто
+              </li>
+              <li>
+                <MdOutlineDiscount /> Скидки дня
+              </li>
+              <li>Блог</li>
+              <li>Бренды</li>
+              <div className={scss.line}></div>
+              <li>Шины</li>
+              <li>Диски</li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
       <AutoPodbor_modal isOpen={isModalOpen} onClose={handleModalClose} />
-      </div>
       <CatalogModal isOpen={isOpen} />
     </header>
   );
