04bb38c8ca96b204b3bcf76e76b7f8d8c3e3c7c5
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-02-04 10:54:54 +0600

    add site bar filter

---
+++ src/app/(root)/data-results/page.tsx
@@ -0,0 +1,10 @@
+import FilterSiteBar from '@/components/ui/filter/sideBar/FilterSiteBar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <> <FilterSiteBar/> </>
+  )
+}
+
+export default page
