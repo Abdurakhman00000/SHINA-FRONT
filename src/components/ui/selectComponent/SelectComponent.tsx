3768f15efe42347be0ce3d38d3342a3914c6f15c
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-02-08 18:24:12 +0600

    fix some things

--- src/components/ui/selectComponent/SelectComponent.tsx
+++ src/components/ui/selectComponent/SelectComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import scss from "./SelectComponent.module.scss";
 
