2e59307061908ef11189c4bf3462a7ad2b6d36fa
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-04-09 20:05:57 +0600

    refactor: fixed some elements

--- src/components/ui/cards/product_card/Product_card.tsx
+++ src/components/ui/cards/product_card/Product_card.tsx
@@ -5,22 +5,19 @@ import Link from "next/link";
 import { IoMdHeart } from "react-icons/io";
 import { GiCheckMark } from "react-icons/gi";
 import { useLoacalStorageData } from "@/store/useLocalStorageData";
-
 interface Product_cardProps {
   tyre: Tyres;
   handleremoveFavorites?: (id: number) => void;
   isFavoriPage?: boolean;
 }
-
 const Product_card = ({
   tyre,
   handleremoveFavorites,
   isFavoriPage,
 }: Product_cardProps) => {
   const { comparesData, setCompareTyres } = useLoacalStorageData();
-
   useEffect(() => {
-    const compareTyres = JSON.parse(localStorage.getItem("compares") || "[]"); 
+    const compareTyres = JSON.parse(localStorage.getItem("compares") as string);
     setCompareTyres(compareTyres);
   }, []);
 
@@ -37,37 +34,16 @@ const Product_card = ({
         setCompareTyres(compares);
       }
       localStorage.setItem("compares", JSON.stringify(compares));
-    if (!tyre) return;
-  
-    const compareTyres = localStorage.getItem("compares");
-    let compares: Tyres[] = [];
-  
-    try {
-      const parsed = compareTyres ? JSON.parse(compareTyres) : [];
-      compares = Array.isArray(parsed) ? parsed : [];
-    } catch (e) {
-      console.error("Ошибка парсинга compares:", e);
-      compares = [];
     }
-  
-    if (compares.some((com) => com.id === tyre.id)) {
-      compares = compares.filter((com) => com.id !== tyre.id);
-    } else {
-      compares.push(tyre);
-    }
-  
-    localStorage.setItem("compares", JSON.stringify(compares));
-    setCompareTyres(compares);
   };
 
   if (!tyre) {
     return (
       <div>
-        <p>Loading...</p>
+        <p>Loding...</p>
       </div>
     );
   }
-
   return (
     <div>
       <div className={scss.card}>
@@ -79,7 +55,7 @@ const Product_card = ({
               className={scss.remove_favorite}
               onClick={() => {
                 if (handleremoveFavorites) {
-                  handleremoveFavorites(tyre.id);
+                  handleremoveFavorites(tyre?.id);
                 }
               }}
             />
@@ -107,7 +83,7 @@ const Product_card = ({
             </Link>
 
             <button className={scss.icon} onClick={handleAddCompare}>
-              {Array.isArray(comparesData) && comparesData.some((com) => com.id === tyre.id) ? (
+              {comparesData.some((com) => com.id === tyre.id) ? (
                 <GiCheckMark />
               ) : (
                 <RiScales3Line />
@@ -119,5 +95,4 @@ const Product_card = ({
     </div>
   );
 };
-
 export default Product_card;
